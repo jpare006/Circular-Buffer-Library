eaaf063d78ebaa840d0871fbddb4ac0448dbf470
jpare006 <jpare006@ucr.edu>
2019-12-18 22:40:34 -0800

    Use template for class, seperate declaration of class (.h) and implementation of class (.ipp)

--- Cpp_implementation/include/CircularBuffer.h
+++ Cpp_implementation/include/CircularBuffer.h
@@ -1,16 +1,18 @@
 #ifndef _CIRCULAR_BUFFER_H_
 #define _CIRCULAR_BUFFER_H_
 
+template <class T>
 class circular_buffer
 {
-public:
-	uint8_t size();
-	circular_buffer(size_t s);
-	~circular_buffer();
-private:
-	uint8_t max_size;
-	uint8_t * buffer;
+	public:
+		size_t size();
+		circular_buffer(size_t s);
+		~circular_buffer();
+	private:
+		T max_size;
+		T * buffer;
 };
 
+#include "CircularBuffer.ipp"
 
 #endif /* _CIRCULAR_BUFFER_H_ */
